2b9deaebd83d778d4e4a54d3f1c11dad1c49e8b1
davidbreau@live.fr <davidbreau@live.fr>
2025-02-02 20:02:41 +0100

    Switch com

---
+++ variables.sh
@@ -0,0 +1,22 @@
+### 🌐 VARIABLES AZURE
+
+# 📂 RESOURCE GROUP
+export RG_NAME="RG_DBREAU"
+
+# 🌊 DATA LAKE
+export ADLS_NAME="adlsbreau"
+export CNTNR_NAME="data"
+
+# 🆔 SERVICE PRINCIPAL
+# DATA LAKE
+export SP_NAME_1="sp-datalake-dbreau"
+export SP_ROLE_1="Storage Blob Data Contributor"
+# KEYVAULT
+export SP_NAME_2="sp-keyvault-dbreau"
+
+# 🔒 KEYVAULT
+export KV_NAME="keyvault-dbreau"
+export KV_SECRET_NAME="sp-keyvault-dbreau-password"
+
+# 📍 LOCATION
+export LOCATION="FranceCentral"
